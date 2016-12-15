bc9ca8d501c9d55567725123dfab29bed7749662
jresinas <jresinas@emergya.com>
2016-12-15 14:46:01 +0100

    Update for Redmine 3.X

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
-# Plugin's routes
-RedmineApp::Application.routes.draw do
-	#match '/wiki/copy' => 'wiki#copy'
-	resources :projects do
-	    resources :wiki, :only => [:copy], :as => 'wiki_page' do
-	      member do
-	        get 'copy'
-	        post 'copy'
-	      end
-	    end
-	end
-
-	match 'projects/:id/copy_wiki', :to => 'projects#copy_wiki'
+resources :projects do
+    resources :wiki, :only => [:copy], :as => 'wiki_page' do
+      member do
+        get 'copy'
+        post 'copy'
+      end
+    end
 end
+
+match 'projects/:id/copy_wiki', :to => 'projects#copy_wiki', :via => [:get, :post]
